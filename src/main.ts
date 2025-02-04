d91315a91f06c2dac25a1479554cd3dee2a95cec
CDOM201 <carlos@carlosdaniel.info>
2025-02-03 23:03:03 -0700

    Posthog implementation

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,16 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import posthog from 'posthog-js';
+import { environment } from './environments/environment';
+
+posthog.init(
+  environment.POSTHOG_KEY,
+  {
+    api_host: environment.POSTHOG_HOST,
+    person_profiles: 'always'
+  }
+)
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
