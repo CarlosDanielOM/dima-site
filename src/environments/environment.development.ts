536ffcef51c2c00f26cc100b5849dc4b26d787a8
CDOM201 <carlos@carlosdaniel.info>
2025-03-31 19:19:52 -0700

    idk

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,7 @@
+export const environment = {
+    production: false,
+    POSTHOG_KEY : "phc_ApcLd2XbNHavPCcyD9fFDVHxs7cCBPozWmSBFTqugfP",
+    POSTHOG_HOST : "https://us.i.posthog.com",
+    DIMA_API: "https://api.domdimabot.com",
+    CLIENT_ID: "jl9k3mi67pmrbl1bh67y07ezjdc4cf"
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    production: true,
+    POSTHOG_KEY : "phc_ApcLd2XbNHavPCcyD9fFDVHxs7cCBPozWmSBFTqugfP",
+    POSTHOG_HOST : "https://us.i.posthog.com",
+    DIMA_API: "https://api.domdimabot.com"
+};
