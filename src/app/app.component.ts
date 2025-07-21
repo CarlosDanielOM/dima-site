9b80ae372db67242ba96bac240f98dad5d67cc57
CDOM201 <carlos@carlosdaniel.info>
2025-07-21 13:29:29 -0700

    Dark Mode first attempt

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,13 @@ import { Event, NavigationEnd, Router, RouterOutlet } from '@angular/router';
 import posthog from 'posthog-js';
 import { filter, Observable } from 'rxjs';
 import { ToastComponent } from './toast/toast.component';
+import { ThemesService } from './services/themes.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ToastComponent],
+  imports: [RouterOutlet, ToastComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -17,7 +19,8 @@ export class AppComponent {
   navigationEnd: Observable<NavigationEnd>;
 
   constructor(
-    public router: Router
+    public router: Router,
+    public themeService: ThemesService
   ) {
     this.navigationEnd = router.events.pipe(
       filter((event: Event) => event instanceof NavigationEnd)
